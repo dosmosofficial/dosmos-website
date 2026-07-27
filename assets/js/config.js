3cbf1575f14ede94ba06561900ce3a2df986c603
dosmosofficial <dosmosid@gmail.com>
2026-07-28 03:55:46 +0700

    config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 window.DOSMOS_CONFIG = {
-  supabaseUrl: "https://qtwllrwbttjpocxgkzn.supabase.co",
+  supabaseUrl: "https://qtwlllrwbttjpocxgkzn.supabase.co",
   supabasePublishableKey: "sb_publishable_IJdLg2CqqlZTCOog7cORXQ_gNWMfAiV"
 };
